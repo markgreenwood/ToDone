b5f40986b533fdc4d35f0ba512a43616d83556c2
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2017-12-01 15:32:40 -0800

    Added placeholder for Add Task form

---
+++ src/Components/AddTask.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AddTaskWrapper from './style/AddTaskWrapper';
+
+const AddTask = () => {
+  return <AddTaskWrapper>Add Task form goes here</AddTaskWrapper>;
+};
+
+export default AddTask;
--- src/Components/App.js
+++ src/Components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 // import './App.css';
 import tasks from '../data/tasks';
 import TaskList from './TaskList';
+import AddTask from './AddTask';
 
 function App() {
   return (
     <div>
       <h1>Your Tasks</h1>
+      <AddTask />
       <TaskList tasks={tasks} />
     </div>
   );
