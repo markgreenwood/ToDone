12556e0e37e539a2e4f1af7e632c78a822ede442
Mark Greenwood <mrkjgreenwood@gmail.com>
2017-12-02 14:40:21 -0800

    Added to task data structure, added moment Date formatting

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ListItemWrapper from './style/ListItemWrapper';
+import moment from 'moment';
 
 const ListItem = ({ task }) => {
   return (
     <ListItemWrapper>
       <span><input className="completed" type="checkbox" /></span>
       <span className="description">{task.description}</span>
-      <span className="duedate">{task.duedate}</span>
+      <span className="duedate">{moment(task.duedate).format('l')}</span>
     </ListItemWrapper>
   );
 };
--- src/data/generate-data.js
+++ src/data/generate-data.js
@@ -1,24 +1,51 @@
 const fs = require('fs');
 const path = require('path');
+// const moment = require('moment');
 
 const tasks = [
   {
     id: '1',
     description: 'Do this',
-    duedate: '2018-04-01',
-    complete: false
+    duedate: new Date('2018-04-01'),
+    complete: false,
+    context: {
+      project: '',
+      type: '',
+      location: ''
+    }
   },
   {
     id: '2',
     description: 'Do that',
-    duedate: '2017-12-25',
-    complete: false
+    duedate: new Date('2017-12-25'),
+    complete: false,
+    context: {
+      project: '',
+      type: '',
+      location: ''
+    }
   },
   {
     id: '3',
     description: 'Save the world',
-    duedate: '2017-11-30',
-    complete: false
+    duedate: new Date('2017-11-30'),
+    complete: false,
+    context: {
+      project: '',
+      type: '',
+      location: ''
+    }
+  },
+  {
+    id: '4',
+    description: 'Go to the moon',
+    duedate: new Date('1969-07-20'),
+    complete: true,
+    context: {
+      project: 'Beat the Russians to the Moon',
+      type: '',
+      location: 'Moon'
+    }
   }
 ];
 
